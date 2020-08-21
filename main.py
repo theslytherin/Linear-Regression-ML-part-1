68aa4855ce2ee8339b00e606b3eb6cab779bbcab
theslytherin <anuskasinha1200@gmail.com>
2020-08-21 15:31:20 +0530

    Final Commit

--- main.py
+++ main.py
@@ -13,4 +13,18 @@ predict = "G3" #lables
 X=np.array(data.drop([predict], 1))
 Y = np.array(data[predict])
 
-X_train, Y_train, X_test, Y_test = sklearn.model_selection.train_test_split((X,Y, test_size = 0.1))
+X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(X,Y, test_size=0.1)
+
+linear= linear_model.LinearRegression()
+
+linear.fit(X_train, Y_train)
+acc =linear.score(X_test, Y_test)
+
+print(acc)
+print("Coefficient: \n", linear.coef_)
+print("Intercept: \n", linear.intercept_)
+
+predictions = linear.predict(X_test)
+
+for x in range(len(predictions)):
+    print(round(predictions[x]), X_test[x], Y_test[x])
